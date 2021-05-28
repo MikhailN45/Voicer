0994ab3ef563522df3b3642af4a7f05b67c42154
MikhailN45 <shelove6391@gmail.com>
2021-05-28 09:53:47 +0300

    add Room-based database for recordings

---
+++ app/src/androidTest/java/com/mikhailn45/voicer/RecordDatabaseTest.kt
@@ -0,0 +1,47 @@
+package com.mikhailn45.voicer
+
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.mikhailn45.voicer.database.RecordDatabase
+import com.mikhailn45.voicer.database.RecordDatabaseDao
+import com.mikhailn45.voicer.database.RecordingItem
+import junit.framework.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+import java.lang.Exception
+import kotlin.jvm.Throws
+
+@RunWith(AndroidJUnit4::class)
+class RecordDatabaseTest {
+
+    private lateinit var recordDatabaseDao: RecordDatabaseDao
+    private lateinit var database: RecordDatabase
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        database = Room.inMemoryDatabaseBuilder(context, RecordDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+        recordDatabaseDao = database.recordDatabaseDao
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        database.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testDatabase() {
+        recordDatabaseDao.insert(RecordingItem())
+        val getCount = recordDatabaseDao.getCount()
+        assertEquals(getCount, 1)
+    }
+}
